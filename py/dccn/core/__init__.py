640e654aec670263f2d2f4cc5179bcf6e1e8d3eb
XheCarpenXer <xhecarpenxer@gmail.com>
2026-05-07 09:22:43 -0500

    Revise header comments with copyright details
    Updated header comments to include copyright and licensing information.

--- py/dccn/core/__init__.py
+++ py/dccn/core/__init__.py
@@ -1,5 +1,21 @@
-# DCCN core subpackage
-# GPL-3.0-or-later
+# DCCN — Deterministic Convergent Compute Network
+# Copyright (C) 2026 James Chapman, xhecarpenxer
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Repository: https://github.com/DeterministicConvergentComputeNetwork/DCCN
+# Contact: xhecarpenxer@gmail.com
 
 from .canonicalize import canonicalize, canonical_json
 from .hash import canonical_hash, sha256_str, sha256_bytes
