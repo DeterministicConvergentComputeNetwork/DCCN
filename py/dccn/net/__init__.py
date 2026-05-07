15e5939b875193197d0c23d610f2ce8411859ce8
XheCarpenXer <xhecarpenxer@gmail.com>
2026-05-07 09:23:58 -0500

    Revise header comments in __init__.py
    Updated header comments to include licensing and repository information.

--- py/dccn/net/__init__.py
+++ py/dccn/net/__init__.py
@@ -1,4 +1,24 @@
-# DCCN net subpackage
+# DCCN — Deterministic Convergent Compute Network
+# Copyright (C) 2026 James Chapman, xhecarpenxer
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Repository: https://github.com/DeterministicConvergentComputeNetwork/DCCN
+# Contact: xhecarpenxer@gmail.com
+
+"""DCCN networking subpackage."""
+
 from .peer import Peer
 
 __all__ = ["Peer"]
